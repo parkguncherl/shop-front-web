e6f565977fa7413c9880cfb5ec7edf18dca8e50c
Park Junsung <junsungpark774@gmail.com>
2026-05-22 16:16:42 +0900

    UnderIsland 최초 버전

---
+++ components/common/wrapper/UnderIsland.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './UnderIsland.module.scss';
+
+interface IslandProps {
+  children: React.ReactNode;
+  spread: boolean;
+  stylesOnNorm?: React.CSSProperties;
+  stylesOnSpread?: React.CSSProperties;
+}
+
+export default function UnderIsland({ children, spread, stylesOnNorm, stylesOnSpread }: IslandProps) {
+  return (
+    <div
+      className={`${styles.underIsland} ${spread ? styles.active : ''}`}
+      // style={
+      //   spread
+      //     ? {
+      //         ...(stylesOnSpread != undefined ? stylesOnSpread : defaultStylesOnSpread),
+      //         // backgroundColor: '#f5f5f5',
+      //         // display: 'flex',
+      //         // alignItems: 'center',
+      //         // justifyContent: 'center',
+      //       }
+      //     : { ...(stylesOnNorm != undefined ? stylesOnNorm : defaultStylesOnNorm) }
+      // }
+    >
+      <div className={styles.island}>spreaded</div>
+      <div className={styles.content}>{children}</div>
+      {/*{spread ? <div className={styles.spread}>{children}</div> : <div className={styles.collapsed}>{children}</div>}*/}
+    </div>
+  );
+}
